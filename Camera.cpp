c4a1c9bb736f22d161b3325d4c4fc7e58c021e37
Jorge Korgut Junior <jorge.korgut-junior@insa-lyon.fr>
2025-08-22 18:58:30 +0200

    :white_check_mark: Done!

--- Camera.cpp
+++ Camera.cpp
@@ -22,22 +22,53 @@ Camera::Camera(float posX, float posY, float posZ, float upX, float upY, float u
     updateCameraVectors();
 }
 
+glm::mat4 LookAt(glm::vec3 position, glm::vec3 target, glm::vec3 worldUp)
+{
+    glm::vec3 zaxis = glm::normalize(position - target);
+    // 3. Get positive right axis vector
+    glm::vec3 xaxis = glm::normalize(glm::cross(glm::normalize(worldUp), zaxis));
+    // 4. Calculate camera up vector
+    glm::vec3 yaxis = glm::cross(zaxis, xaxis);
+
+    glm::mat4 translation = glm::mat4(1.0f); // Identity matrix by default
+    translation[3][0] = -position.x; // Fourth column, first row
+    translation[3][1] = -position.y;
+    translation[3][2] = -position.z;
+    glm::mat4 rotation = glm::mat4(1.0f);
+    rotation[0][0] = xaxis.x; // First column, first row
+    rotation[1][0] = xaxis.y;
+    rotation[2][0] = xaxis.z;
+    rotation[0][1] = yaxis.x; // First column, second row
+    rotation[1][1] = yaxis.y;
+    rotation[2][1] = yaxis.z;
+    rotation[0][2] = zaxis.x; // First column, third row
+    rotation[1][2] = zaxis.y;
+    rotation[2][2] = zaxis.z;
+
+    return rotation * translation; // Remember to read from right to left (first translation then rotation)
+}
+
 glm::mat4 Camera::GetViewMatrix()
 {
-    return glm::lookAt(Position, Position + Front, Up);
+    return LookAt(Position, Position + Front, Up);
+    //return glm::lookAt(Position, Position + Front, Up);
 }
 
 void Camera::ProcessKeyboard(Camera_Movement direction, float deltaTime)
 {
+
+    glm::vec3 fpsFront(glm::dot(Front, glm::vec3(1.0f, 0.0f, 0.0f)), 0, glm::dot(Front, glm::vec3(0.0f, 0.0f, 1.0f)));
+    glm::vec3 fpsRight(glm::dot(Right, glm::vec3(1.0f, 0.0f, 0.0f)), 0, glm::dot(Right, glm::vec3(0.0f, 0.0f, 1.0f)));
+
     float velocity = MovementSpeed * deltaTime;
     if (direction == FORWARD)
-        Position += Front * velocity;
+        Position += fpsFront * velocity;
     if (direction == BACKWARD)
-        Position -= Front * velocity;
+        Position -= fpsFront * velocity;
     if (direction == LEFT)
-        Position -= Right * velocity;
+        Position -= fpsRight * velocity;
     if (direction == RIGHT)
-        Position += Right * velocity;
+        Position += fpsRight * velocity;
 }
 
 void Camera::ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch)
