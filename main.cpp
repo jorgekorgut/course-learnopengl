e87a68db7159bfba65233a37d20c0375f392d4fd
Jorge Korgut Junior <jorge.korgut-junior@insa-lyon.fr>
2025-08-24 19:26:41 +0200

    Chapter light casters done.

--- main.cpp
+++ main.cpp
@@ -188,6 +188,19 @@ int main()
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
     };
 
+    glm::vec3 cubePositions[] = {
+    glm::vec3(0.0f,  0.0f,  0.0f),
+    glm::vec3(2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3(2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3(1.3f, -2.0f, -2.5f),
+    glm::vec3(1.5f,  2.0f, -2.5f),
+    glm::vec3(1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
     // Creates VAO
     unsigned int cubeVAO;
     glGenVertexArrays(1, &cubeVAO);
@@ -276,7 +289,20 @@ int main()
         glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f);
         ourShader.setVec3("light.ambient", ambientColor);
         ourShader.setVec3("light.diffuse", diffuseColor);
+
+
+        /*
+        // Point light
+        ourShader.setFloat("light.constant", 1.0f);
+        ourShader.setFloat("light.linear", 0.09f);
+        ourShader.setFloat("light.quadratic", 0.032f);
         ourShader.setVec3("light.position", lightPos);
+        */
+
+        ourShader.setVec3("light.position", camera.Position);
+        ourShader.setVec3("light.direction", camera.Front);
+        ourShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+        ourShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
 
 
         glm::mat4 view = camera.GetViewMatrix();
@@ -284,34 +310,40 @@ int main()
         ourShader.setMat4("view", view);
         ourShader.setMat4("projection", projection);
 
-        model = glm::mat4(1.0f);
-        ourShader.setMat4("model", model);
-		normalMatrix = glm::transpose(glm::inverse(model)); // Update normal matrix only in case of non-uniform scaling
-		ourShader.setMat3("normalMatrix", normalMatrix);
-
         ourShader.setVec3("viewPos", camera.Position);
-        
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, diffuseMap);
 
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, specularMap);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            ourShader.setMat4("model", model);
 
-        glBindVertexArray(cubeVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+            normalMatrix = glm::transpose(glm::inverse(model)); // Update normal matrix only in case of non-uniform scaling
+            ourShader.setMat3("normalMatrix", normalMatrix);
 
-        lightCubeShader.use();
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, diffuseMap);
+
+            glActiveTexture(GL_TEXTURE1);
+            glBindTexture(GL_TEXTURE_2D, specularMap);
+
+            glBindVertexArray(cubeVAO);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+		lightCubeShader.use();
 		lightCubeShader.setMat4("view", view);
 		lightCubeShader.setMat4("projection", projection);
 		lightCubeShader.setVec3("lightColor", lightColor);
-        
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.5f)); // a smaller cube
+		lightCubeShader.setMat4("model", model);
         glBindVertexArray(lightCubeVAO);
-       
-        model = glm::mat4(1.0f);
-        model = glm::translate(model, lightPos);
-        model = glm::scale(model, glm::vec3(0.2f));
-        lightCubeShader.setMat4("model", model);
         glDrawArrays(GL_TRIANGLES, 0, 36);
+
         
         glBindVertexArray(0);
 
--- shader.fs
+++ shader.fs
@@ -16,12 +16,39 @@ struct Material {
   
 uniform Material material;
 
+/* point light
 struct Light {
-    vec3 position;
+    vec3 position;  
+  
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+	
+    float constant;
+    float linear;
+    float quadratic;
+}; 
+*/
+
+/* directional light
+struct Light {
+    vec3 direction;
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
 };
+*/
+
+struct Light {
+    vec3  position;
+    vec3  direction;
+    float cutOff;
+    float outerCutOff;
+    
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};    
 
 uniform Light light;  
 
@@ -43,6 +70,25 @@ void main()
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     vec3 specular = light.specular * (spec * vec3(texture(material.specular, TexCoords)));  
 
-    vec3 result = (ambient + diffuse + specular);
+    /*
+    // point light with attenuation
+    float distance    = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + 
+    		        light.quadratic * (distance * distance));
+    
+    ambient *= attenuation;
+    diffuse *= attenuation;
+    specular *= attenuation;
+    */
+
+    float theta     = dot(lightDir, normalize(-light.direction));
+    float epsilon   = light.cutOff - light.outerCutOff;
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+    
+    diffuse  *= intensity;
+    specular *= intensity;
+    
+    vec3 result = ambient + diffuse + specular;
+
     FragColor = vec4(result, 1.0);
 }   
